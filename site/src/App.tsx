8cd3a180d340936aac166f453bad9fcc97c63469
nmorse <morse.nate@gmail.com>
2023-08-23 23:39:32 -0400

    lppy try

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -14,53 +14,17 @@ function App() {
     const svg = svgRef?.current?.innerHTML;
     if (svg) {
       const blob = new Blob([svg], { type: "image/svg+xml" });
-      downloadBlob(blob, `bespokeSvg${count + 1}-of-50.svg`);
+      downloadBlob(blob, `loopy-${count + 1}-of-50.svg`);
     }
   }, []);
   
   const startPt = [30, 30]
-  const columns = 27
-  const rows = 17
-  // const interp = interpreter(`${count % 50 + 11} seedrandom random drop 
-  // [dup 0 > [1 - [f n] [n f leap f n] pounce itimes] [drop drop] if-else] [itimes] compose
-  // [${columns} ${rows} * 2 / floor] [midway] compose
-  // [midway - abs -1 * midway +] [center] compose
-  // [[i] [random .5 - i center * 40 midway / * random .5 - i center * 40 midway / * [] cons cons] pounce] [rpt] compose
-  // [[i] [i rpt i rpt i rpt i rpt i rpt i rpt [] cons cons cons cons cons cons] pounce] [rptset] compose
-  // [[i] [i rptset i rptset i rptset [] cons cons cons] pounce] [squigle] compose
-  // [squigle] ${columns} ${rows} * itimes
-  // `);
-  // const interp = interpreter(`${count % 50 + 11} seedrandom random drop 
-  // [dup 0 > [1 - [f n] [n f leap f n] pounce itimes] [drop drop] if-else] [itimes] compose
-  // [${columns} ${rows} * 2 / floor] [midway] compose
-  // [midway - abs -1 * midway +] [center] compose
-  // [[i] [random .5 - i center * 40 midway / * random .5 - i center * 40 midway / * [] cons cons] pounce] [rpt] compose
-  // [[i] [i rpt i rpt i rpt i rpt i rpt i rpt [] cons cons cons cons cons cons] pounce] [rptset] compose
-  // [[i] [i rptset i rptset i rptset [] cons cons cons] pounce] [squigle] compose
-  // [ [[a b c]] [[a b c][a b c]] pounce ] [tweak] compose
-  // 200 squigle [tweak] ${columns} ${rows} * times
-  // `);
-  // const interp = interpreter(`${count % 50 + 11} seedrandom random drop 
-  // [dup 0 > [1 - [f n] [n f leap f n] pounce itimes] [drop drop] if-else] [itimes] compose
-  // [${columns} ${rows} * 2 / floor] [midway] compose
-  // [midway - abs -1 * midway +] [center] compose
-  // [[i] [random .5 - i center * 40 midway / * random .5 - i center * 40 midway / * [] cons cons] pounce] [rpt] compose
-  // [[i] [i rpt i rpt i rpt i rpt i rpt i rpt [] cons cons cons cons cons cons] pounce] [rptset] compose
-  // [[i] [i rptset i rptset i rptset [] cons cons cons] pounce] [squigle] compose
-  // [[[random 0.5 - +] map] map][updateE] compose
-  // [ [[a b c]] [[a b c][a b c]]  pounce pop updateE push ] [tweak] compose
-  //   100 squigle [tweak] ${columns} ${rows} * times
-  // `);
-  const interp = interpreter(`${count % 50 + 11} seedrandom random drop 
-  [dup 0 > [1 - [f n] [n f leap f n] pounce itimes] [drop drop] if-else] [itimes] compose
-  [${columns} ${rows} * 2 / floor] [midway] compose
-  [midway - abs -1 * midway +] [center] compose
-  [[i] [random .5 - i center * 40 midway / * random .5 - i center * 40 midway / * [] cons cons] pounce] [rpt] compose
-  [[i] [i rpt i rpt i rpt i rpt i rpt i rpt [] cons cons cons cons cons cons] pounce] [rptset] compose
-  [[i] [i rptset i rptset i rptset [] cons cons cons] pounce] [squigle] compose
-  [[[[random 0.5 - +] map] map] map][updateE] compose
-  [ dup  updateE ] [tweak] compose
-    100 squigle [tweak] ${columns} ${rows} * times
+  const columns = 1
+  const rows = 5
+  const interp = interpreter(`
+  [3 -1 -1 1 1 -3] [3 3 -1 -1 1 -5]
+   [1 1 1 1 1 -5] [1 3 3 -1 -1 -5]
+   [1 1 3 -3 -1 -1]
   `);
 
   let result = interp.next();
@@ -72,16 +36,18 @@ function App() {
   let allPaths = []
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows; y++) {
-      allPaths.push([translate(startPt, [x * 20, y * 20]), value.stack[x * rows + y]])
+      console.log(value.stack[x * rows + y])
+      allPaths.push([translate(startPt, [x * 20 + 10, y * 20 + 10]), value.stack[x * rows + y]])
     }
   }
+  //console.log(allPaths)
   return <>
   <div ref={svgRef}>
       <svg style={{ backgroundColor: "#ddd", strokeLinecap: "round",
     strokeLinejoin: "round" }} width="604" height="384" xmlns="http://www.w3.org/2000/svg">
         <g id="Layer_1" stroke="null">
           <title>Layer 1</title>
-          {allPaths.map((p: any, i) => <path fill="none" strokeWidth="0.7" key={`path_${i}`} id={`path_${i}`} d={makePathDString(p[0], p[1])} stroke="#000" />)}
+          {allPaths.map((p: any, i) => <path fill="none" strokeWidth="0.7" key={`path_${i}`} id={`path_${i}`} d={makeLoopyPathDString(p[0], p[1])} stroke="#000" />)}
         </g>
       </svg>
       </div>
@@ -114,6 +80,30 @@ const makePathDString = (start: number[], curves: number[][][]) => {
   const makePtsString = (pta: number[][]) => pta.map(pt => mkPtStr(pt)).join(" ")
   const allCurves = curves.map((c: number[][]) => `c${makePtsString(c)}`)
   return `m${start.join(",")} ${allCurves.join(" ")}`
+}
+
+const makeLoopyPathDString = (start: number[], curves: number[]) => {
+  const mkPtStr = (pt: number[], scale: number): string => pt.map((n) => n * scale).join(" ")
+  const makePtsString = (jump: number) => {
+    //console.log("jump", jump)
+    const pta = [[0, 0], [0, 10], [10, 10], [0, 0], [10, 0], [10, -10], [20, -10], [20, 0]]
+    return pta.map(pt => mkPtStr(pt, jump)).join(", ")
+  }
+  const allCurves = curves.map((c: number, i) => ` M${[start[0]+i*20, start[1]].join(" ")} c${makePtsString(c)}`)
+
+  console.log(allCurves.join(" "))
+  return allCurves.join(" ")
+}
+
+const makeLoopyPathDStringMock = (start: number[], curves: number[]) => {
+  const mkPtStr = (pt: number[], scale: number): string => pt.map((n) => n * scale).join(",")
+  const makePtsString = (jump: number) => {
+    //console.log("jump", jump)
+    const pta = [[0, 0], [0, 1], [1, 1], [1, 0]]
+    return pta.map(pt => mkPtStr(pt, jump)).join(" ")
+  }
+  const allCurves = curves.map((c: number) => `c${makePtsString(c)}`)
+  return `M${start.join(",")} C 70 80, 110 80, 110 60`
 }
 
 function downloadBlob(blob: Blob | MediaSource, filename: string) {
